6be78ef158c079cf30b25a10c16dfc25e99a2b21
kaufmanno <olivier.kaufmann@umons.ac.be>
2017-03-19 09:11:50 +0100

    Update testpysftp.py

--- ardas/testpysftp.py
+++ ardas/testpysftp.py
@@ -3,7 +3,7 @@ __author__ = 'su530201'
 import pysftp as sftp
 
 try:
-    s = sftp.Connection(host='10.107.10.148', username='karag', password='edas44')
+    s = sftp.Connection(host='xxx', username='xxx', password='xxx')
     remote_path='/home/karag/raw.dat.gz'
     local_path='/home/su530201/raw.dat.gz'
     s.get(remote_path, local_path)
