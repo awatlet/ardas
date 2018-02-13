5b5cee0db77abf4311a18a51b37b70e93f65ac9b
kaufmanno <olivier.kaufmann@umons.ac.be>
2018-02-13 10:02:52 +0100

    use 7 character in git commit number

--- ardas/get_git_version.py
+++ ardas/get_git_version.py
@@ -3,9 +3,13 @@ import os
 
 
 def get_version():
+    """ Get the current version from git info
+
+    :return: a string foramted as branch_name | last_named_tag-number_of_commits_ahead-commit_number
+    """
     dir = os.path.dirname(__file__)
     branch = subprocess.check_output('cd ' + dir + '; git rev-parse --abbrev-ref HEAD', shell=True)
-    version = subprocess.check_output('cd ' + dir + '; git describe --long --dirty --abbrev=7 --tags', shell=True)
+    version = subprocess.check_output('cd ' + dir + '; git describe --long --dirty --abbrev=6 --tags', shell=True)
     return branch.decode('ascii')[:-1] + ' | ' + version.decode('ascii')[:-1]
 
 if __name__ == '__main__':
