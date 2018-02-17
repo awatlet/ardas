8f7fbd2b5df8fc3000156b84eb0b70e544d3d792
kaufmanno <olivier.kaufmann@umons.ac.be>
2018-02-17 20:33:20 +0100

    testing super

--- ardas/sensor_tools.py
+++ ardas/sensor_tools.py
@@ -13,7 +13,7 @@ try:
             return W1ThermSensor.get_available_sensors(types)
 
         def __init__(self, sensor_type=None, sensor_id=None):
-            W1ThermSensor.__init__(sensor_type=sensor_type, sensor_id=sensor_id)
+            super(W1ThermSensor, self).__init__(sensor_type=sensor_type, sensor_id=sensor_id)
 
 except:
     from ardas.fake_sensor import FakeTempSensor
