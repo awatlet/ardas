553be77227cb36eb3eb4738b034ebc425a66a472
kaufmanno <olivier.kaufmann@umons.ac.be>
2022-02-21 12:33:06 +0100

    Added methods for full tank detection

--- ardas/sensor_tools.py
+++ ardas/sensor_tools.py
@@ -3,8 +3,11 @@ import os
 from pathlib import Path
 from pickle import dump, load
 import logging
+import RPi.GPIO as GPIO
+import time
 
 cur_dir = os.path.dirname(os.path.realpath(__file__))
+GPIO.setmode(GPIO.bcm)
 
 
 def polynomial(value, coefs):
@@ -18,30 +21,51 @@ def polynomial(value, coefs):
 
     result = coefs[-1]
     for i in range(-2, -len(coefs) - 1, -1):
-        result = result * value + coefs[i]
+        result = result * value[-1] + coefs[i]
+    result=result[0]
     assert isinstance(result, float)
     return result
 
 
-def running_average(n):
+def running_average(x, coefs):
     """Compute a running average (not centered)
 
-    :param n: number of former samples used to compute the running average
-    :return: evaluation of polynomial for the given value of the variable (freq)
+    :param x: np.array of samples used to compute the running average
+    :return: evaluation of the running average
     :rtype: float
     """
 
-    l = []
-    average = None
-    while True:
-        new_elt = yield average
-        if len(l) == n:
-            del l[0]
-        l.append(new_elt)
-        if len(l) > 0:
-            average = sum(l) / len(l)
-        else:
-            average = np.nan
+    return np.mean(x)
+
+
+def activate_pin(pin, delay, safe_pins=(12, 6, 13, 16, 19, 20, 26, 21)):
+    """ Activates a pin of the raspberry pi in output mode for the number of seconds sepcified in delay"""
+    if pin in safe_pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(GPIO.HIGH)
+        logging.info('Activating ' + pin + ' for ' + delay + ' seconds.')
+        time.sleep(delay)
+        GPIO.output(pin, GPIO.LOW)
+        logging.info('Deactivating ' + pin + '.')
+    else:
+        logging.warning('Pin number ' + pin + ' is not in safe_pins. Skipped activation pin '+ pin + '.')
+
+
+def running_median(x, coefs):
+    """Compute a running average (not centered)
+
+    :param x: np.array of samples used to compute the running median
+    :return: evaluation of the running median
+    :rtype: float
+    """
+
+    return np.median(x)
+
+
+def open_valve_if_full(x, threshold=1600.,  pin=12, delay=30., safe_pins=(12)):
+    logging.debug('Sensor freq. :' + x + 'Hz')
+    if running_median(x, None) > threshold:
+        activate_pin(pin, delay, safe_pins)
 
 
 def load_sensor(sensor_id):
@@ -58,10 +82,13 @@ def load_sensor(sensor_id):
 
 class FMSensor(object):
     def __init__(self, sensor_id='0000', processing_method=polynomial, processing_parameters=(0., 1., 0., 0., 0.),
-                 quantity='freq.', units='Hz', output_format='%11.4f', log_output=True):
+                 quantity='freq.', units='Hz', output_format='%11.4f', short_term_memory=1, log_output=True):
         self.sensor_id = sensor_id
         self.processing_method = processing_method
         self.processing_parameters = processing_parameters
+        self._value = np.empty((short_term_memory,1))
+        self.short_term_memory = short_term_memory
+        self._value[:] = np.nan
         self.quantity = quantity
         self.units = units
         self.output_format = output_format
@@ -78,8 +105,21 @@ class FMSensor(object):
         self.__sensor_id = str(val[:4])
 
     @property
+    def value(self):
+        """ Gets and sets sensor value
+        """
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        assert isinstance(val, float)
+        self._value = np.roll(self._value, -1)
+        self._value[-1] = val
+
+
+    @property
     def units(self):
-        """ Gets and sets sensor units
+        """ Gets and sets sensor value
         """
         return self.__units
 
@@ -87,23 +127,27 @@ class FMSensor(object):
     def units(self, val):
         self.__units = val
 
-    def output(self, value):
+    def output(self, value=None):
         """Outputs an output computed using the processing method and parameters
 
         :return: processed quantity
         :rtype: float
         """
 
+        if value is None:
+            value = self.value
         output = self.processing_method(value, self.processing_parameters)
         return output
 
-    def output_repr(self, value):
+    def output_repr(self, value=None):
         """Gets a representation of the output
 
         :return: representation of the processed quantity
         :rtype: string
         """
 
+        if value is None:
+            value = self.value[-1]
         try:
             s = self.output_format + ' ' + self.units
             calibrated_output = s % self.output(value)
@@ -129,7 +173,19 @@ class UncalibratedFMSensor(FMSensor):
 
 
 if __name__ == '__main__':
-    t = polynomial(25000, [-16.9224032438, 0.0041525221, -1.31475837290789e-07, 2.39122208189129e-12,
-                           -1.72530800355418e-17])
-    print(t)
-    print(cur_dir)
+    #t = polynomial(25000, [-16.9224032438, 0.0041525221, -1.31475837290789e-07, 2.39122208189129e-12,
+    #                       -1.72530800355418e-17])
+    #print(t)
+    #print(cur_dir)
+    # sensor = FMSensor(sensor_id='9999', processing_method=running_average, processing_parameters=None,
+    #                   short_term_memory=3, log_output=False)
+    # while True:
+    #     reply = input('Please enter sensor value')
+    #     sensor.value = float(reply)
+    #     print(sensor.output())
+    sensor = FMSensor(sensor_id='9999', processing_method=polynomial,
+                      processing_parameters=(-16.922, 0.0041525221, -1.314e-07, 2.391e-12, -1.725e-17),
+                      log_output=False)
+    sensor.value = 25000.
+    print(sensor.output())
+
